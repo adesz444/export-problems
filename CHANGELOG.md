433f0ea515d7d9a793350dec0d1abbf8281f21e5
Adam Szabo <adam.szabo@cosmoconsult.com>
2022-05-09 18:28:17 +0200

    export functionality added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,7 @@
 
 All notable changes to the "export-problems" extension will be documented in this file.
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
 ## [Unreleased]
 
+## [1.0.0] - 2022-05-09
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import path = require('path');
 import * as vscode from 'vscode';
+import { Uri } from "vscode";
+const fs = require('fs');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "export-problems" is now active!');
+	let exporter = new Exporter();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('export-problems.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Export Problems!');
+	let disposable = vscode.commands.registerCommand('export-problems.exportProblems', () => {
+		exporter.exportProblems();
 	});
 
 	context.subscriptions.push(disposable);
@@ -24,3 +15,87 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+class Problem {
+	constructor() {
+		this.path = '';
+		this.severity = '';
+		this.code = '';
+		this.target = Uri.parse('my:uri', true);
+		this.message = '';
+		this.startLine = 0;
+		this.startCharacter = 0;
+	}
+
+	path: string;
+	severity: string;
+	code: string | number;
+    target: Uri;
+    message: string;
+    startLine: number;
+    startCharacter: number;
+
+	public toString(): string {
+		return this.formatOutput(this.path) + ',' + this.formatOutput(this.severity) + ',' + this.formatOutput(this.code.toString()) + ',' + this.formatOutput(this.message)+ ',' + this.formatOutput(this.startLine.toString()) + ','  + this.formatOutput(this.startCharacter.toString()) + ',' + this.formatOutput(this.target.toString());
+	}
+
+	private formatOutput(input: string): string {
+		if (input.includes(",")) {
+			return '"' + input + '"';	
+		} else {
+			return input;
+		}
+	}
+}
+
+class Exporter {
+	public exportProblems() {
+		let diagnostics = vscode.languages.getDiagnostics();
+
+		let filePath = '';
+		if (vscode.workspace.workspaceFolders !== undefined){
+			filePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		} else {
+			vscode.window.showErrorMessage('Export Problems: Working folder not found.');
+		}
+
+		let problems: Problem[] = [];
+
+		for (var [currUri, currDiagnostics] of diagnostics) {
+			for (let currDiagnostic of currDiagnostics) {
+				let currProblem: Problem = new Problem();
+				
+				if (currUri.fsPath.startsWith(filePath)){
+					currProblem.path = currUri.fsPath.substring(filePath.length + 1);
+				} else {
+					currProblem.path = currUri.fsPath;
+				}
+
+				currProblem.severity = vscode.DiagnosticSeverity[currDiagnostic.severity];
+				
+                if (typeof currDiagnostic.code === "string" || typeof currDiagnostic.code === "number") {
+					currProblem.code = currDiagnostic.code;
+				} else if (currDiagnostic.code?.value !== undefined) {
+                    currProblem.code = currDiagnostic.code.value;
+                    currProblem.target = currDiagnostic.code.target;
+                }
+
+				currProblem.message = currDiagnostic.message;
+				currProblem.startLine = currDiagnostic.range.start.line + 1;
+				currProblem.startCharacter = currDiagnostic.range.start.character + 1;
+
+				problems.push(currProblem);
+			}
+		}
+
+		let header = 'File,Severity,Code,Message,Line,Column,Target\r\n';
+
+		fs.writeFile(path.join(filePath, 'problems.csv'), header + problems.join('\r\n'), (err: any) => {
+            if (err) {
+                vscode.window.showErrorMessage(err.toString());
+            }
+        });
+
+		vscode.window.showInformationMessage('Export Problems: csv exported!');
+	}
+}
